fc37ffd30fc8ad2503d1a12b8b0c1f1bf818ca9b
Prateek Karnal <prateekkarnal77@gmail.com>
2017-05-22 14:56:47 +0530

    Add files via upload

---
+++ FastExponentiation.cpp
@@ -0,0 +1,13 @@
+// Takes a base 'b', exponent 'e' and modulo 'm'
+// m should be less than 3e9
+// Complexity O(log(e))
+ll modex(ll b,ll e,ll m){
+	ll r=1;
+	b%=m;
+	while(e){
+		if(e&1)	r=(r*b)%m;
+		e>>=1;
+		b=(b*b)%m;
+	}
+	return r;
+}
