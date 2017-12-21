89d5f4ca97f0806d47fdbc507a3d269685f48dd1
shaurya gupta <shauryab98@outlook.com>
2017-12-21 23:13:37 +0530

    Sublime

---
+++ Snippets/Snippet.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T> using V = vector<T>;
+template<typename T, typename V> using P = pair<T, V>;
+template<typename T> using min_heap = priority_queue<T, V<T>, greater<T>>;
+
+using LL = long long;
+using ll = LL;
+using LD = long double;
+using ld = long double;
+
+#define fi first
+#define ff first
+#define se second
+#define ss second
+#define pp push_back
+#define pb pp
+#define endl '\n'
+#define SYNC std::ios::sync_with_stdio(false);  cin.tie(NULL);
+#define ALL(v) v.begin(), v.end()
+#define FOR0(i,n) for(int i=0, _##i=(n); i<_##i; ++i)
+#define FOR(i,l,r) for(int i=(l), _##i=(r); i<_##i; ++i)
+#define FORD(i,l,r) for(int i=(r), _##i=(l); --i>=_##i; )
+#define rep(i,a) FOR0(i, a)
+#define repn(i,a) FOR(i, 1, a + 1)
+#define REP(i, n) rep(i, n)
+#define REPN(i, n) repn(i, n)
+#define SZ(a) ((int)((a).size()))
+#define mp make_pair
+#define dzx cerr << "here";
+#define her cerr << "HERE "
+#define pii pair<int,int>
+#define ii pii
+#define en(v) *(--v.end())
+
+const int MOD = (int)1e9 + 7, inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+
+int32_t main() {SYNC;
+    
+    return 0;
+}
