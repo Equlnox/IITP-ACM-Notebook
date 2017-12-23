cec0a162046890a2f5cdcbfd0f137228e6215e5b
shaurya gupta <shauryab98@outlook.com>
2017-12-23 07:04:56 +0530

    aho

--- code/aho-corasick.cpp
+++ code/aho-corasick.cpp
@@ -1,19 +1,26 @@
-typedef vector<vi> vvi;
 const int N = 2000 * 1005;
 map<char, int> go[N];
-int par[N], occ[N], sz = 1, fail[N], ans[1005] = {};
-char pc[N];
-void add(string& s, int i ) {
+int par[N], occ[N], sz = 1, fail[N];
+char parc[p];
+void add(string& s, int i) {
     int cur = 0;
     for(char c : s) {
-        auto it = go[cur].find(c);
-        if(it == go[cur].end()) {
-            par[sz] = cur;
-            pc[sz] = c;
-            cur = go[cur][c] = sz++;
-        } else cur = it->ss;
+        if(!go[cur][c])
+            parc[sz]=c,par[sz]=cur,cur=go[cur][c] = ++sz;
+        else
+            cur=go[cur][c];
     }
     occ[cur]++;
+}
+int getlink(int p) {
+    if(!link[p]) {
+        if(p==1 || par[p]==1) link[p]=1;
+        else {
+            link[p]=go(getlink(par[p],parc[p]));
+            occ[p] += occ[link[p]];
+        }
+    }
+    return link[p];
 }
 int nxt(int cur, char c) {
     auto it = go[cur].find(c);
@@ -22,6 +29,7 @@ int nxt(int cur, char c) {
             if(!cur || !par[cur]) return fail[cur] = 0;
             fail[cur] = nxt(par[cur], c);
             occ[cur] += occ[fail[cur]]];
-        } else return fail[cur];
+        }
+        return fail[cur];
     } else return it->ss;
 }
