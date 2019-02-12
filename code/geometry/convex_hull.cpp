b7c3f16487c043c7021eca48d94e397af1296bab
i_love_ayrawhsia_nodnat <bhavit.sharma804@gmail.com>
2019-02-13 00:08:58 +0530

    made changes to convex_hull implementation. It's now more robust with duplicate points

--- code/geometry/convex_hull.cpp
+++ code/geometry/convex_hull.cpp
@@ -1,32 +1,38 @@
-vector<Point> half_hull(vector<Point> &pts,int t){
-    vector<Point> hull;
-    hull.pb(pts[0]);
-    hull.pb(pts[1]);
-    for(int i=2;i<n;i++){
-        while((int)hull.size()>1){
-            Point p1=hull[(int)hull.size()-2];
-            Point p2=hull.back();
-            if((((p1-pts[i])*(p2-pts[i]))*t)>=0){
-                hull.pop_back();
-            }
-            else
-                break;
-        }
-        hull.pb(pts[i]);
+/*
+ * This implementation passes this https://open.kattis.com/problems/convexhull
+ */
+long long ccw(Point a, Point b, Point c) { return (b - a) * (c - b); }
+
+vector<Point> convex_hull(vector<Point>& pts)
+{
+  sort(begin(pts), end(pts), [](const Point& a, const Point& b) {
+    return (a.x < b.x || (a.x == b.x && a.y < b.y));
+  });
+
+  int n = (int)pts.size();
+  if (n == 1)
+    return pts;
+  vector<Point> ans;
+  int sz = (int)ans.size();
+
+  for (int i = 0; i < n; ++i, ++sz) {
+    while (sz > 1 && (ccw(ans[sz - 2], ans[sz - 1], pts[i]) <= 0)) {
+      sz--;
+      ans.pop_back();
     }
-    return move(hull);
-}
-vector<Point> convex_hull(vector<Point> &pts){
-    sort(pts.begin(), pts.end(),[](Point &a,Point &b){
-        if(a.x==b.x)
-            return a.y<b.y;
-        return a.x<b.x;
-    });
-    vector<Point> uh,lh;
-    uh=half_hull(pts,1);
-    lh=half_hull(pts,-1);
-    lh.pop_back();
-    reverse(lh.begin(), lh.end());
-    uh.insert(uh.end(),lh.begin(), lh.end());
-    return move(uh);
+    ans.push_back(pts[i]);
+  }
+
+  int temp = sz;
+  for (int i = n - 2; i >= 0; --i, ++sz) {
+    while (sz > temp && (ccw(ans[sz - 2], ans[sz - 1], pts[i]) <= 0)) {
+      sz--;
+      ans.pop_back();
+    }
+    ans.push_back(pts[i]);
+  }
+
+  while ((int)ans.size() > 1 && ans[0] == ans.back())
+    ans.pop_back();
+  return ans;
 }
